21bc632fbe5003c90de42f31f73bad0562960795
mtralka <mtralka@umd.edu>
2021-01-03 22:17:45 -0500

    🎨 black

--- data/download_lodes.py
+++ data/download_lodes.py
@@ -1,10 +1,11 @@
 import requests
 import pandas as pd
 
-BASE = 'https://lehd.ces.census.gov/data/lodes/LODES7/'
+BASE = "https://lehd.ces.census.gov/data/lodes/LODES7/"
 
-def download_lodes(states, kind='od', year=2018):
-    ''' 
+
+def download_lodes(states, kind="od", year=2018):
+    """
     Get data from the LODES database. Designed to be run from this directory.
     Inputs:
         - states = single string or list of strings of states to get
@@ -14,12 +15,12 @@ def download_lodes(states, kind='od', year=2018):
     Resources:
         See https://lehd.ces.census.gov/data/lodes/LODES7/LODESTechDoc7.5.pdf
         for more details.
-    '''
+    """
 
     for state in states:
-        qrystart = BASE + '/' + state + '/' + kind + '/'
-        datafile = state + '_' + kind + '_main_JT00_' + year + '.csv.gz'
+        qrystart = BASE + "/" + state + "/" + kind + "/"
+        datafile = state + "_" + kind + "_main_JT00_" + year + ".csv.gz"
         r = requests.get(qrystart + datafile)
 
-        with open(datafile, 'wb') as f:
+        with open(datafile, "wb") as f:
             f.write(r.content)
