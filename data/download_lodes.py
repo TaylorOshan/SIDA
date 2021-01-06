f44e6c0c74ad13a9c299b2e36bcff38664baa3a8
thoffman1 <tylerhoff1@gmail.com>
2021-01-06 13:12:45 -0500

    add data collection scripts

--- data/download_lodes.py
+++ data/download_lodes.py
@@ -1,25 +1,40 @@
 import requests
-import pandas as pd
 
 BASE = 'https://lehd.ces.census.gov/data/lodes/LODES7/'
 
-def download_lodes(states, kind='od', year=2018):
+def download_lodes(state, contained=True, year=2018):
     ''' 
     Get data from the LODES database. Designed to be run from this directory.
     Inputs:
-        - states = single string or list of strings of states to get
-        - kind   = 'od', 'rac', or 'wac' -- the kind of file to get
-        - year   = year for data to be queried from
+        - state = single string of state to get
+        - year  = year for data to be queried from
 
     Resources:
         See https://lehd.ces.census.gov/data/lodes/LODES7/LODESTechDoc7.5.pdf
         for more details.
     '''
+    if contained:
+        prefix = 'main'
+        filenames = dict.fromkeys(['od', 'wac', 'rac'])
+    else:
+        prefix = 'aux'
+        filenames = dict.fromkeys(['od'])
 
-    for state in states:
+    ending = str(year) + '.csv.gz'  # ending of all filenames
+    
+    for kind in filenames.keys():
         qrystart = BASE + '/' + state + '/' + kind + '/'
-        datafile = state + '_' + kind + '_main_JT00_' + year + '.csv.gz'
+        datafile = state + '_' + kind + '_'
+
+        if kind == 'od':
+            datafile += prefix + '_JT00_' + ending
+        if kind == 'rac' or kind == 'wac':
+            datafile += 'S000_JT00_' + ending
+        filenames[kind] = datafile
+
         r = requests.get(qrystart + datafile)
 
         with open(datafile, 'wb') as f:
             f.write(r.content)
+
+    return filenames
